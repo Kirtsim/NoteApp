b7dbb8d436156073ff6d920857e885de9a968301
Kharos <filipmistrik@gmail.com>
2017-07-27 07:04:49 +0100

    initial commit

---
+++ app/src/main/java/fm/kirtsim/kharos/noteapp/MainActivity.java
@@ -0,0 +1,10 @@
+package fm.kirtsim.kharos.noteapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kharos on 27/07/2017
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/fm/kirtsim/kharos/noteapp/NoteApplication.java
@@ -0,0 +1,10 @@
+package fm.kirtsim.kharos.noteapp;
+
+import android.app.Application;
+
+/**
+ * Created by kharos on 27/07/2017
+ */
+
+public class NoteApplication extends Application {
+}
