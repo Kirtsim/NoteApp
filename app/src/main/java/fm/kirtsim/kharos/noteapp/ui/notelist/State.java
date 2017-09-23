725d4fa4fdee76f2e40af7e1e1fd464298516d9b
Kharos <filipmistrik@gmail.com>
2017-09-23 16:18:24 +0100

    make State class implement Parcelable

--- app/src/main/java/fm/kirtsim/kharos/noteapp/ui/notelist/State.java
+++ app/src/main/java/fm/kirtsim/kharos/noteapp/ui/notelist/State.java
@@ -1,11 +1,14 @@
 package fm.kirtsim.kharos.noteapp.ui.notelist;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by kharos on 11/09/2017
  */
 
-@SuppressWarnings("unused")
-class State {
+@SuppressWarnings({"unused", "WeakerAccess"})
+class State implements Parcelable{
 
     static final int START = 0;
     static final int DEFAULT = 1;
@@ -17,6 +20,7 @@ class State {
     private static final int STATE_COUNT = 7;
 
     private int state;
+    private boolean wasRestored;
 
     State() {
         this(START);
@@ -24,16 +28,42 @@ class State {
 
     State(int initialState) {
         setState(initialState);
+        setWasRestored(false);
     }
+
+    State(Parcel parcel) {
+        state = parcel.readInt();
+        wasRestored = parcel.readByte() != 0;
+    }
+
+    public static final Creator<State> CREATOR = new Creator<State>() {
+        @Override
+        public State createFromParcel(Parcel in) {
+            return new State(in);
+        }
+
+        @Override
+        public State[] newArray(int size) {
+            return new State[size];
+        }
+    };
 
     int getState() {
         return state;
+    }
+
+    boolean wasRestored() {
+        return wasRestored;
     }
 
     void setState(int newState) {
         if (STATE_COUNT < newState || newState < 0)
             throw new IllegalArgumentException("the set state is undefined");
         state = newState;
+    }
+
+    void setWasRestored(boolean restored) {
+        wasRestored = restored;
     }
 
     boolean isInStartState() { return state == START; }
@@ -59,4 +89,15 @@ class State {
     }
 
     boolean isInNewNoteState() { return state == NEW_NOTE; }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(state);
+        dest.writeByte((byte) (wasRestored ? 1 : 0));
+    }
 }
